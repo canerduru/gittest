73f83bfef01d67f0e992db0b1cd2cc49a8c434d4
owner <canerduru@ymail.com>
2020-09-30 22:38:42 +0300

    first commit

---
+++ gittestproject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gittestproject
+//
+//  Created by owner on 9/30/20.
+//  Copyright © 2020 Caner Duru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("git")
+        print("1")
+    }
+
+
+}
+
